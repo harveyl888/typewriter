b3f221c96b914b8dd772681ec5237e91b361df4a
Harvey Lieberman <harvey.lieberman@gmail.com>
2025-01-26 16:44:44 -0500

    add additional parameters

--- _extensions/typewriter/typewriter.lua
+++ _extensions/typewriter/typewriter.lua
@@ -32,20 +32,68 @@ return {
         return pandoc.RawBlock('html', '<div></div>')
       end
 
+      -- parameters
+      local t_params = {} 
+
+      -- cursor
+      if typewriter.cursor then
+        table.insert(t_params, "cursor: '" .. pandoc.utils.stringify(typewriter.cursor) .. "'")
+      end
+
+      -- delay
+      if typewriter.delay then
+        if typewriter.delay == "natural" then
+          table.insert(t_params, "delay: 'natural'")
+        else
+          table.insert(t_params, "delay: " .. pandoc.utils.stringify(typewriter.delay))
+        end
+      end
+
+      -- deleteSpeed
+      if typewriter.deleteSpeed then
+        if typewriter.deleteSpeed == "natural" then
+          table.insert(t_params, "deleteSpeed: 'natural'")
+        else
+          table.insert(t_params, "deleteSpeed: " .. pandoc.utils.stringify(typewriter.deleteSpeed))
+        end
+      end
+
+      -- loop
+      if typewriter.loop then
+        if typewriter.loop == true then
+          table.insert(t_params, "loop: true")
+        else
+          table.insert(t_params, "loop: false")
+        end
+      end
+
+      -- pauseFor
+      if typewriter.pauseFor then
+        table.insert(t_params, "pauseFor: " .. pandoc.utils.stringify(typewriter.pauseFor))
+      end
+
+      local num_params = #t_params
+      local params
+      if num_params > 0 then
+        params = table.concat(t_params, ", ")
+      else
+        params = ""
+      end
+      
       ensureHtmlDeps()
 
       local rtn = string.format([[
         <div class="typewriter">
           <span class="typewriter-prefix">%s</span>
-          <span id="typewritertext" class="typewriter-text"></span>
+          <span id="typewritertext"></span>
         </div>
 
         <script>
             document.addEventListener('DOMContentLoaded', function() {
-                new Typewriter('#typewritertext', {strings: [%s],autoStart: true,});
+                new Typewriter('#typewritertext', {strings: [%s], autoStart: true, wrapperClassName: 'typewriter-strings', cursorClassName: 'typewriter-cursor', %s});
             });
         </script>
-      ]], prefix, strings)
+      ]], prefix, strings, params)
 
       return pandoc.RawBlock('html', rtn)
     end
