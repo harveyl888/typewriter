c0d996220a3e19b46c8172d5ae8c62e25c2d3972
Harvey Lieberman <harvey.lieberman@gmail.com>
2025-01-26 12:35:14 -0500

    pass first parameter and test

--- _extensions/typewriter/typewriter.lua
+++ _extensions/typewriter/typewriter.lua
@@ -1,5 +1,43 @@
+local function ensureHtmlDeps()
+  quarto.doc.add_html_dependency({
+    name = 'typewriter',
+    version = '1.0.0',
+    scripts = {'assets/js/core.js'}
+  })
+end
+
+
 return {
   ['typewriter'] = function(args, kwargs, meta) 
-    return pandoc.Str("Hello from Typewriter!")
+
+    if quarto.doc.is_format("html:js") then
+
+      local typewriter = meta.typewriter
+
+      if typewriter.strings then
+        strings = ""
+        for _, s in ipairs(typewriter.strings) do
+          strings = strings .. '"' .. pandoc.utils.stringify(s) .. '"' .. ","
+        end
+        strings = string.sub(strings, 1, -2)
+      else
+        return pandoc.RawBlock('html', '<div></div>')
+      end
+
+      print(strings)
+
+      ensureHtmlDeps()
+
+      local rtn = string.format([[
+        <div id='typewriter'></div>
+        <script>
+            document.addEventListener('DOMContentLoaded', function() {
+                new Typewriter('#typewriter', {strings: [%s],autoStart: true,});
+            });
+        </script>
+      ]], strings)
+
+      return pandoc.RawBlock('html', rtn)
+    end
   end
 }
